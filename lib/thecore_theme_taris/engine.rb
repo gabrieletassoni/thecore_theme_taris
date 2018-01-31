42d661694db037f3d878beedc824b9e2b5fd0137
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2018-01-31 22:41:56 +0100

    Added static assets manager and better dependencies

--- lib/thecore_theme_taris/engine.rb
+++ lib/thecore_theme_taris/engine.rb
@@ -1,4 +1,11 @@
 module ThecoreThemeTaris
   class Engine < ::Rails::Engine
+    # This code makes the static assets present in this 
+    # engine available to main app, even if this gem is 
+    # just a dependency of another engine
+    initializer 'thecore_theme_taris.load_static_assets' do |app|
+      puts "Loading static assets for #{root}"
+      app.middleware.use ::ActionDispatch::Static, "#{root}/app"
+    end
   end
 end
--- lib/thecore_theme_taris/version.rb
+++ lib/thecore_theme_taris/version.rb
@@ -1,3 +1,3 @@
 module ThecoreThemeTaris
-  VERSION = '1.1.1'
+  VERSION = '1.1.3'
 end
