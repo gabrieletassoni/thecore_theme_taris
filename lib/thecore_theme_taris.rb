2964bf9011a3096268e6348e06f4a7f06c3d000d
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2018-08-30 12:50:59 +0200

    fix: add require for dependency

--- lib/thecore_theme_taris.rb
+++ lib/thecore_theme_taris.rb
@@ -1,4 +1,6 @@
 require 'rails_admin_ui_layout_taris'
+require 'rails_admin_ui_dashboard_blocks'
+require 'rails_admin_ui_index_cards'
 require 'thecore_ui_layout_taris_website'
 
 require "thecore_theme_taris/engine"
--- lib/thecore_theme_taris/version.rb
+++ lib/thecore_theme_taris/version.rb
@@ -1,3 +1,3 @@
 module ThecoreThemeTaris
-  VERSION = '1.1.3'
+  VERSION = '1.1.4'
 end
