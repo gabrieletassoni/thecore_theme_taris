d2974dd1609ea1a5acf6d169946549e5c71033ba
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2018-09-28 16:28:56 +0200

    Remove debug code

--- lib/thecore_theme_taris/engine.rb
+++ lib/thecore_theme_taris/engine.rb
@@ -4,7 +4,7 @@ module ThecoreThemeTaris
     # engine available to main app, even if this gem is 
     # just a dependency of another engine
     initializer 'thecore_theme_taris.load_static_assets' do |app|
-      puts "Loading static assets for #{root}"
+      # puts "Loading static assets for #{root}"
       app.middleware.use ::ActionDispatch::Static, "#{root}/app"
     end
   end
